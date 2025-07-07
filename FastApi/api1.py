cadca7d4d1c3cecdbbfca2fcfc43f7db8b1b2c6b
Luís Gustavo <luis.araujo.sesisenaisp@gmail.com>
2025-07-06 21:16:38 -0300

    Estudo https FastAPI

--- FastApi/api1.py
+++ FastApi/api1.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app =FastAPI()
 jogadores = {
@@ -28,3 +29,28 @@ def get_jogar_time(time:str):
         if jogadores[i]["time"] == time:
             return jogadores[i]
         return {"Dados":"Nao encontrado"}
+class jogador(BaseModel):
+    nom:str
+    idade:int
+    time:str
+class atualiza_jogador(BaseModel):
+    nom:str
+    idade:int
+    time:str
+@app.post("/inserir_jogador/{id_jogador}")
+def inserir_jogadores(id_jogador:int,jogador: jogador):
+    if id_jogador in jogadores:
+        return {"Mensagem":"Jogador ja existe"}
+    jogadores[id_jogador] = jogador
+    return jogadores[id_jogador]
+class NoneJogaor(BaseModel):
+    nom:str = None
+    idade:int = None
+    tiem:str = None
+@app.delete("/deletear_jogadores{id_jogador}")
+def delete_jogador(id_jogador:int,jogador:NoneJogaor):
+    jogadores[id_jogador] = jogador
+@app.put("/att_dados{id_jogador}")
+def att_dados(id_jogador:int,jogador:atualiza_jogador):
+    jogadore[id_jogador] = jogador    
+    
